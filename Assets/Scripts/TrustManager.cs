c95861c0f82d7535467bd21450347cb9ab72a0d8
Gabriel <gabrielroger.chartroulle@gmail.com>
2022-05-22 19:18:38 -0400

    Correction max

--- Assets/Scripts/TrustManager.cs
+++ Assets/Scripts/TrustManager.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement; 
 
 public class TrustManagemer : MonoBehaviour
 {
@@ -21,13 +21,13 @@ public class TrustManagemer : MonoBehaviour
 
 
         // Ce nombre, compris entre -1 et 1, mesure la performance du joueur par rapport à ce qui était attendu
-        float deathScore = float.MaxValue((expectedDeaths - nbDeathsCurrentTurn) / expectedDeaths, -1);
+        float deathScore = Mathf.Max((expectedDeaths - nbDeathsCurrentTurn) / expectedDeaths, -1);
 
         // On modifie la confiance des humains en fonction de la performance
         // Si on ne perd personne à ce tour, la confiance est multipliée par 1.5
         // Si on perd au moins deux fois plus de personnes que prévu, la confiance est divisée par 2
         // Si on perd autant de gens que prévu, la confiance ne bouge pas
-        trustLevel = float.MaxValue(float.MinValue((1 + deathScore/2) * trustLevel, 100), 0);
+        trustLevel = Mathf.Max(Mathf.Min((1 + deathScore/2) * trustLevel, 100), 0);
 
         // On calcule le nombre de morts attendu au prochain tour
         expectedDeaths = trustLevel * (100 - deaths);
