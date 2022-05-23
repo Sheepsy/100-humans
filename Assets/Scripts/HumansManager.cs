c6ed4b91602b7686d4f233224a59e7143dff3998
Sheepsy <jehan.rouby@outlook.com>
2022-05-22 20:40:48 -0400

    Humans spawn

---
+++ Assets/Scripts/HumansManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class HumansManager
+{
+    public static int nbTrust = 50;
+    public static int nbUntrust = 50;
+    public static bool[] trust = new bool[100];
+}
