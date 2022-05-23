66dcf5c979aef2400bb6c200633fc2c4a2223f8a
ShadowsSwords <conti.shadow@gmail.com>
2022-05-23 10:35:25 -0400

    finish labyrinth design

--- Assets/Scripts/Moving_Labyrinth.cs
+++ Assets/Scripts/Moving_Labyrinth.cs
@@ -5,18 +5,21 @@ using UnityEngine;
 public class Moving_Labyrinth : MonoBehaviour
 {
     [SerializeField] Transform pos;
+    [SerializeField] Transform endPos;
     [SerializeField] float speed = 0.01f;
+
+    private float _endPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        _endPos = endPos.position.y - pos.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (pos.position.y > -10.0f) {
+        if (pos.position.y > -_endPos) {
             pos.position -= new Vector3(0, speed, 0);
         }
     }
